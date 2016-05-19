da211d6d7074b6adc69d77fafbf5877b099b7a69
Kanit Wongsuphasawat <kanitw@gmail.com>
2016-05-19 16:34:10 -0700

    basic demo

--- src/cql.ts
+++ src/cql.ts
@@ -1,7 +1,8 @@
 export const version = '__VERSION__';
-
-// import * as cqlGenerate from './generate';
+import * as cqlGenerate from './generate';
+import * as cqlSchema from './schema';
 // import * as cqlUtil from './util';
 
-// export const generate = cqlGenerate;
+export const generate = cqlGenerate.generate;
+export const schema = cqlSchema;
 // export const util = cqlUtil;
