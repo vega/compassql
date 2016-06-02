ae99dfa809ee93d7a233fd534462d9c2e75c8e37
Kanit Wongsuphasawat <kanitw@gmail.com>
2016-06-01 23:16:01 -0700

    correct set cql.ts

--- src/cql.ts
+++ src/cql.ts
@@ -1,8 +1,18 @@
 export const version = '__VERSION__';
 import * as cqlGenerate from './generate';
+import * as cqlGroup from './group';
+import * as cqlModel from './model';
+import * as cqlProperty from './property';
+import * as cqlQuery from './query';
 import * as cqlSchema from './schema';
-// import * as cqlUtil from './util';
+import * as cqlStats from './stats';
+import * as cqlUtil from './util';
 
 export const generate = cqlGenerate.generate;
+export const group = cqlGroup;
+export const model = cqlModel;
+export const property = cqlProperty;
+export const query = cqlQuery;
 export const schema = cqlSchema;
-// export const util = cqlUtil;
+export const stats = cqlStats;
+export const util = cqlUtil;
