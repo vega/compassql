9262a912b9840514be6d0bee3870caccaebc8911
Kanit "Ham" Wongsuphasawat <kanitw@gmail.com>
2016-07-30 07:48:53 -0700

    Correct isEnumSpec method, which causes `fo/scale_round` to fail

--- test/enumspec.test.ts
+++ test/enumspec.test.ts
@@ -1,7 +1,48 @@
 import {assert} from 'chai';
-import {initEnumSpec} from '../src/enumspec';
+import {initEnumSpec, isEnumSpec, SHORT_ENUM_SPEC} from '../src/enumspec';
 
 describe('enumspec', () => {
+  describe('isEnumSpec', () => {
+    it('should return true for an enum spec with name and values', () => {
+      assert(isEnumSpec({
+        name: 'a',
+        values: [1,2,3]
+      }));
+    });
+
+    it('should return true for an enum spec with name.', () => {
+      assert(isEnumSpec({
+        name: 'a'
+      }));
+    });
+
+    it('should return true for an enum spec with values', () => {
+      assert(isEnumSpec({
+        values: [1,2,3]
+      }));
+    });
+
+    it('should return true for a short enum spec', () => {
+      assert(isEnumSpec(SHORT_ENUM_SPEC));
+    });
+
+    it('should return false for a string', () => {
+      assert(!isEnumSpec('string'));
+    });
+
+    it('should return false for a number', () => {
+      assert(!isEnumSpec(9));
+    });
+
+    it('should return false for a boolean value', () => {
+      assert(!isEnumSpec(true));
+    });
+
+    it('should return false for an array', () => {
+      assert(!isEnumSpec([1,2]));
+    });
+  });
+
   describe('initEnumSpec', () => {
     it('should return full enumSpec with other properties preserved', () => {
       const binQuery = initEnumSpec({values: [true, false], maxbins: 30}, 'b1', [true, false]);
