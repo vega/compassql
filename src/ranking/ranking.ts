560b92cf43248182b656b22a4615a18763dbba5c
Kanit Wongsuphasawat <kanitw@gmail.com>
2016-08-05 14:24:24 -0700

    Missing //
    cc: @felixcodes

--- src/ranking/ranking.ts
+++ src/ranking/ranking.ts
@@ -40,7 +40,7 @@ export function rank(group: SpecQueryModelGroup, query: Query, schema: Schema, l
         if (group.items.length > 0) {
           // for chooseBy -- only keep the top-item
           group.items = [group.items[0]];
-        } else {
+        } else { // except when the group is empty
           group.items = [];
         }
       }
