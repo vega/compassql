a34d2a42e82d2554ba658eb5dbd8bfd3cedc076b
Felix Ouk <felix.ouk@gmail.com>
2016-08-25 11:19:34 -0700

    create specM to test comparator factory

--- test/ranking/ranking.test.ts
+++ test/ranking/ranking.test.ts
@@ -5,10 +5,7 @@ import {Type} from 'vega-lite/src/type';
 import {schema} from '../fixture';
 
 import {DEFAULT_QUERY_CONFIG} from '../../src/config';
-import {SHORT_ENUM_SPEC} from '../../src/enumspec';
-import {generate} from '../../src/generate';
-import {SpecQueryModelGroup} from '../../src/model';
-import {SpecQuery} from '../../src/query/spec';
+import {SpecQueryModel, SpecQueryModelGroup} from '../../src/model';
 import {rank, comparatorFactory} from '../../src/ranking/ranking';
 
 import {assert} from 'chai';
@@ -37,53 +34,61 @@ describe('ranking', () => {
 
   describe('comparatorFactory', () => {
     it('should create a comparator that returns a score difference when passed an orderBy array', () => {
-      const specQ: SpecQuery = {
-        mark: SHORT_ENUM_SPEC,
-        encodings: [
-          {
-            channel: SHORT_ENUM_SPEC,
-            bin: SHORT_ENUM_SPEC,
-            aggregate: SHORT_ENUM_SPEC,
-            field: 'Q',
-            type: Type.QUANTITATIVE
-          },
-          {
-            channel: SHORT_ENUM_SPEC,
-            bin: SHORT_ENUM_SPEC,
-            aggregate: SHORT_ENUM_SPEC,
-            field: 'Q1',
-            type: Type.QUANTITATIVE
-          }
-        ]
-      };
-      const answerSet = generate(specQ, schema, DEFAULT_QUERY_CONFIG);
+      const specM1 = SpecQueryModel.build(
+        {
+          mark: Mark.POINT,
+          encodings: [
+            {channel: Channel.X, field: 'Q', type: Type.QUANTITATIVE},
+            {channel: Channel.Y, field: 'Q1', type: Type.QUANTITATIVE},
+          ]
+        },
+        schema,
+        DEFAULT_QUERY_CONFIG
+      );
+
+      const specM2 = SpecQueryModel.build(
+        {
+          mark: Mark.BAR,
+          encodings: [
+            {channel: Channel.X, field: 'N', type: Type.NOMINAL},
+            {channel: Channel.Y, field: 'Q', type: Type.QUANTITATIVE},
+          ]
+        },
+        schema,
+        DEFAULT_QUERY_CONFIG
+      );
+
       const comparator = comparatorFactory(['aggregationQuality', 'effectiveness'], schema, DEFAULT_QUERY_CONFIG);
-      assert.isNumber(comparator(answerSet[1], answerSet[4]));
+      assert.isNumber(comparator(specM1, specM2));
     });
 
     it('should create a comparator that returns a score difference when passed an orderBy string', () => {
-      const specQ: SpecQuery = {
-        mark: SHORT_ENUM_SPEC,
-        encodings: [
-          {
-            channel: SHORT_ENUM_SPEC,
-            bin: SHORT_ENUM_SPEC,
-            aggregate: SHORT_ENUM_SPEC,
-            field: 'Q',
-            type: Type.QUANTITATIVE
-          },
-          {
-            channel: SHORT_ENUM_SPEC,
-            bin: SHORT_ENUM_SPEC,
-            aggregate: SHORT_ENUM_SPEC,
-            field: 'Q1',
-            type: Type.QUANTITATIVE
-          }
-        ]
-      };
-      const answerSet = generate(specQ, schema, DEFAULT_QUERY_CONFIG);
+      const specM1 = SpecQueryModel.build(
+        {
+          mark: Mark.POINT,
+          encodings: [
+            {channel: Channel.X, field: 'Q', type: Type.QUANTITATIVE},
+            {channel: Channel.Y, field: 'Q1', type: Type.QUANTITATIVE},
+          ]
+        },
+        schema,
+        DEFAULT_QUERY_CONFIG
+      );
+
+      const specM2 = SpecQueryModel.build(
+        {
+          mark: Mark.BAR,
+          encodings: [
+            {channel: Channel.X, field: 'N', type: Type.NOMINAL},
+            {channel: Channel.Y, field: 'Q', type: Type.QUANTITATIVE},
+          ]
+        },
+        schema,
+        DEFAULT_QUERY_CONFIG
+      );
+
       const comparator = comparatorFactory('aggregationQuality', schema, DEFAULT_QUERY_CONFIG);
-      assert.isNumber(comparator(answerSet[1], answerSet[4]));
+      assert.isNumber(comparator(specM1, specM2));
     });
   });
 });
