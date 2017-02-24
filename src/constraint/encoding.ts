d2f26cac1a625a9166b6718f0b395f3a34817542
Akshat Shrivastava <akshatshrivastava@hotmail.com>
2017-02-24 15:22:45 -0800

    WIP updating encoding

--- src/constraint/encoding.ts
+++ src/constraint/encoding.ts
@@ -403,7 +403,23 @@ const FIELD_CONSTRAINTS_BY_PROPERTY =
     return index;
   }, new PropIndex<EncodingConstraintModel<FieldQuery>[]>());
 
-export const VALUE_CONSTRAINTS: EncodingConstraintModel<ValueQuery>[] = [].map((ec: EncodingConstraint<ValueQuery>) => new EncodingConstraintModel<ValueQuery>(ec));
+export const VALUE_CONSTRAINTS: EncodingConstraintModel<ValueQuery>[] = [
+  {
+    name: 'doesNotSupportConstantValue',
+    description: 'row, column, x, y, and detail should not work with constant values.',
+    properties: [Property.TYPE, Property.AGGREGATE],
+    allowWildcardForProperties: false,
+    strict: true,
+    satisfy: (valueQ: ValueQuery, _: Schema, __: PropIndex<Wildcard<any>>, ___: QueryConfig) => {
+      // TODO: some aggregate function are actually supported by ordinal
+
+      valueQ.value 
+
+      return true; // no aggregate is okay with any type.
+    }
+  // TODO: minCardinalityForBin
+  }
+].map((ec: EncodingConstraint<ValueQuery>) => new EncodingConstraintModel<ValueQuery>(ec));
 
 export const VALUE_CONSTRAINT_INDEX : {[name: string]: EncodingConstraintModel<ValueQuery>} =
   VALUE_CONSTRAINTS.reduce((m, ec: EncodingConstraintModel<ValueQuery>) => {
