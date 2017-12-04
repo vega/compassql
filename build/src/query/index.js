b56baa5d43418ae9f638bc12ce89606dee750ee3
Kanit Wongsuphasawat <kanitw@gmail.com>
2017-12-03 17:42:20 -0800

    release 0.20.0 65cf9d1727f00c2416e0a66e6885d8a3aee2bcc8

---
+++ build/src/query/index.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.encoding = require("./encoding");
+var normalize_1 = require("./normalize");
+exports.normalize = normalize_1.normalize;
+exports.groupBy = require("./groupby");
+exports.shorthand = require("./shorthand");
+exports.spec = require("./spec");
+exports.transform = require("./transform");
+//# sourceMappingURL=index.js.map
