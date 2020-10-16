93a3a742b2b530cdbe6823b7f8bec4c4fd786c75
Dominik Moritz <domoritz@gmail.com>
2020-10-16 10:44:10 -0700

    chore: remove bower

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,33 +4,26 @@ set -e
 RED='\033[0;31m'
 NC='\033[0m' # No Color
 
-
-# 0.1 Check if jq has been installed
-type jq >/dev/null 2>&1 || { echo >&2 "I require jq but it's not installed.  Aborting."; exit 1; }
-
-# 0.2 check if on master
+# Check if on master
 if [ "$(git rev-parse --abbrev-ref HEAD)" != "master" ]; then
   echo "${RED}Not on master, please checkout master branch before running this script${NC}"
   exit 1
 fi
 
-# 0.3 Check if all files are committed
+# Check if all files are committed
 if [ -z "$(git status --porcelain)" ]; then
-  echo "All tracked files are committed.  Publishing on npm and bower. \n"
+  echo "All tracked files are committed.  Publishing on npm. \n"
 else
   echo "${RED}There are uncommitted files. Please commit or stash first!${NC} \n\n"
   git status
   exit 1
 fi
-# read version
-gitsha=$(git rev-parse HEAD)
-version=$(cat package.json | jq .version | sed -e 's/^"//'  -e 's/"$//')
 
 # swap to head so we don't commit compiled file to master along with tags
 git checkout head
 npm run build
 
-# 1. NPM PUBLISH
+# NPM PUBLISH
 
 npm publish
 # exit if npm publish failed
@@ -39,18 +32,3 @@ if [[ $rc != 0 ]]; then
   echo "${RED} npm publish failed.  Publishing cancelled (Make sure you sure npm run deploy, not yarn run deploy). ${NC} \n\n"
   exit $rc;
 fi
-
-# 2. BOWER PUBLISH
-
-# add the compiled files, commit and tag!
-git add build/compassql* -f
-git add build/src/**/*.js -f
-
-git commit -m "release $version $gitsha"
-git tag -am "Release v$version." "v$version"
-
-# now swap back to the clean master and push the new tag
-git checkout master
-git push --tags
-
-npm run build # rebuild so we have the build files back like before running deploy script.
